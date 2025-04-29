919973e873f0259dd071c52eaa227122326f2fd7
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-04-29 18:46:27 -0500

    update to  5 20

--- test_progs/connectivity_maps.py
+++ test_progs/connectivity_maps.py
@@ -102,7 +102,7 @@ edges_heavy_hex = [
 
 #TRAPPED ION
 # 20 x 5
-edges_trapped_ion = [
+edges_trapped_ion_5_20 = [
     # Group 0
     (0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4),
     # Group 1
